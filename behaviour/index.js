772398c0326a056396fd06604da712beab3e0032
Hayder Al Rubaye <hayddex@gmail.com>
2023-02-24 22:04:55 -0600

    first commit

---
+++ behaviour/index.js
@@ -0,0 +1,16 @@
+/*
+	
+
+
+    " The people who are crazy enough to think they can change the world, are the ones who do. "
+	- Apple's Think Different Commercial 1997
+	
+	https://www.youtube.com/watch?v=Rzu6zeLSWq8
+	https://www.youtube.com/watch?v=2B-XwPjn9YY
+	https://www.youtube.com/watch?v=QOlTGA3RE8I
+	
+	Copyright © 2016 HAYDEX Inc. All Rights Reserved.
+	
+	
+
+*/
